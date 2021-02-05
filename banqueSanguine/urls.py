9f7e575672a0d76e8a0155bae3370bdd21f75a3e
billson14 <58440768+billson14@users.noreply.github.com>
2021-02-05 18:25:46 +0000

    Formulaire d'enregistrement modifié

--- banqueSanguine/urls.py
+++ banqueSanguine/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from . import views 
 
 urlpatterns = [
-    path('',include('banque.urls')),
+    path('',views.login,name='login'),
+    path('/Register',views.register,name='register'),
     path('admin/', admin.site.urls),
 ]
